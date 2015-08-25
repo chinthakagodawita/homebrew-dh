019b8df068ba88c3b5d4f6bac01c5dad32ed6b91
Chinthaka Godawita <chin@sitback.com.au>
2015-08-25 12:39:37 +1000

    Added proper version number.

--- Formula/docker-helpers.rb
+++ Formula/docker-helpers.rb
@@ -1,8 +1,11 @@
 class DockerHelpers < Formula
+  DH_VERSION="v0.0.5-alpha.5"
+
   homepage "https://github.com/chinthakagodawita/docker-helpers"
-  url "https://github.com/chinthakagodawita/docker-helpers/archive/v0.0.5-alpha.5.tar.gz"
+  url "https://github.com/chinthakagodawita/docker-helpers/archive/#{DH_VERSION}.tar.gz"
   sha256 "53fec67103b04152ea9b43a62d232215836534bd64c26a59f01a6c9a2a643eb2"
   head "https://github.com/chinthakagodawita/docker-helpers.git", branch: :experimental
+  version DH_VERSION
 
   option "with-vbox", "Build with VirtualBox (via brew cask)"
   option "with-vagrant", "npm will not be installed"
