6f06f66b0b6be4bc79adcbcf42422b622b803f7f
Chinthaka Godawita <chin.godawita@me.com>
2015-09-07 19:30:33 +1000

    Updated dinghy recipe to use latest fork version.

--- Formula/dinghy.rb
+++ Formula/dinghy.rb
@@ -1,21 +1,25 @@
 require 'formula'
 
+DINGHY_VERSION='4.0.1-fork.2'
+
 class Dinghy < Formula
   homepage 'https://github.com/chinthakagodawita/dinghy'
-  head 'https://github.com/chinthakagodawita/dinghy.git', branch: 'machine-experimental'
+  url  'https://github.com/chinthakagodawita/dinghy.git', tag: "v#{DINGHY_VERSION}"
+  head 'https://github.com/chinthakagodawita/dinghy.git', branch: :fork
+  version DINGHY_VERSION
 
   depends_on 'docker'
   depends_on 'docker-machine'
   depends_on 'unfs3'
-  depends_on 'dnsmasq'
 
   def install
     bin.install "bin/dinghy"
+    bin.install "bin/_dinghy_command"
     prefix.install "cli"
   end
 
   def caveats; <<-EOS.undent
-    Run `dinghy up` to bring up the VM and services.
+    Run `dinghy create` to create the VM, then `dinghy up` to bring up the VM and services.
     EOS
   end
 end
